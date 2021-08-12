e558a988b4d10d6da695601f7ebd7f9e3e6cd574
Kushal N <kushalnjnv10@gmail.com>
2021-08-12 15:20:15 +0000

    link update

--- Testing.py
+++ Testing.py
@@ -4,4 +4,4 @@ token = "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"
 
 subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', '{}'.format(commit_message)])
-subprocess.call(['git', 'push', 'https://{}@github.com/user-name/repo.git'.format(token)])
+subprocess.call(['git', 'push', 'https://{}@github.com/kushaln3/vscodelink.git'.format(token)])
