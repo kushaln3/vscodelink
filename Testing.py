097209b375c758f0c35130cabe75db2dcc93c755
Kushal N <kushalnjnv10@gmail.com>
2021-08-12 15:48:25 +0000

    link update

--- Testing.py
+++ Testing.py
@@ -1,6 +1,6 @@
 import subprocess
 commit_message = "link update"
-token = "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"
+token = "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"
 
 subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', '{}'.format(commit_message)])
